d8f909d7c3afb1660997c824b27b99a69afd82df
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2025-01-19 23:35:13 -0300

    Implementação do algoritimo QuickSort.

--- ordenacao/QuickSort.cs
+++ ordenacao/QuickSort.cs
@@ -1,17 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EstruturaDeDadosCsharp.ordenacao;
 
-namespace EstruturaDeDadosCsharp.ordenacao
+public class QuickSort
 {
-    internal class QuickSort
+    public static void OrdenarQuickSort(int[] array, int esquerda, int direita)
     {
+        //verifica se ainda existem itens para ordenar
+        if (esquerda < direita)
+        {
+            int indicePivo = Particionar(array, esquerda, direita);
+
+            //Ordena recursivamente os itens á esquerda do pivô
+            if (indicePivo > 1)
+            {
+                OrdenarQuickSort(array, esquerda, indicePivo - 1);
+            }
+
+            //Agora os itens da direita
+            if (indicePivo + 1 < direita)
+            {
+                OrdenarQuickSort(array, indicePivo + 1, direita);
+            }
+        }
+    }
+
+    //Particionar o array em relação ao pivô
+    private static int Particionar(int[] array, int esquerda, int direita)
+    {
+        // escolhe o primeiro elemento como pivô
+        int pivo = array[esquerda];
+
+        // faz o particionamento enquanto os índices não se cruzam
+        while (true)
+        {
+            // move o índice da esquerda até achar um valor maior ou igual ao pivô
+            while (array[esquerda] < pivo)
+            {
+                esquerda++;
+            }
+
+            while (array[direita] > pivo)
+            {
+                direita--;
+            }
+
+            //confere se os índices não se cruzam
+            if (esquerda < direita)
+            {
+                //se os valores forem iguais, retorna o índice da direita
+                if (array[esquerda] == array[direita]) return direita;
 
 
+                //troca os valores encontrados nos indices esquedo e direito
+                int aux = array[esquerda];
+                array[esquerda] = array[direita];
+                array[direita] = aux;
 
-
-
+            }
+            else
+            {
+                return direita;
+            }
+        }
     }
 }
