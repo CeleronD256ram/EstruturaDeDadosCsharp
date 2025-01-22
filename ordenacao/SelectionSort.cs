2bde9a6571699a842009e299ce93f3fbdfe0ffb4
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2025-01-21 23:24:41 -0300

    Alteração no tipo de dado do vetor.

--- ordenacao/SelectionSort.cs
+++ ordenacao/SelectionSort.cs
@@ -2,5 +2,25 @@ namespace EstruturaDeDadosCsharp.ordenacao;
 
 public class SelectionSort
 {
-    
+    public static void OrdenarSelection(double[] array)
+    {
+        int n = array.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int menorElemento = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[menorElemento] > array[j])
+                {
+                    menorElemento = j;
+                }
+            }
+
+            double temp = array[i];
+            array[i] = array[menorElemento];
+            array[menorElemento] = temp;
+        }
+    }
 }
